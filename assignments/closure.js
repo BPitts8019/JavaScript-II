3a8b9e3696d18cd915c8ab6aaaeb712ebe386b2e
BPitts8019 <bradleyj.pitts@gmail.com>
2019-06-28 17:23:54 -0700

    closure.js - Completed the stretch challeges, and updated the readme.

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,4 +1,5 @@
 // ==== Challenge 1: Write your own closure ====
+console.log("==== Challenge 1: Write your own closure ====");
 // Write a simple closure of your own creation.  Keep it simple!
 const trippleIt = (inputNum=1) => {
    const innerFunc = () => {
@@ -20,16 +21,63 @@ console.log(trippleIt(15)); //45
 
 
 // ==== Challenge 2: Create a counter function ====
+console.log("==== Challenge 2: Create a counter function ====");
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+   // Return a function that when invoked increments and returns a counter variable.
+   let count = 0;
+
+   return () => ++count;
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+const newCounter = counter();
+console.log(newCounter()); // 1
+console.log(newCounter()); // 2
+console.log(newCounter()); // 3
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
+console.log("==== Challenge 3: Create a counter function with an object that can increment and decrement ====");
 const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
+   // Return an object that has two methods called `increment` and `decrement`.
+   // `increment` should increment a counter variable in closure scope and return it.
+   // `decrement` should decrement the counter variable and return it.
+   let counter = 0;
+
+   return {
+      increment: () => ++counter,
+      decrement: () => --counter
+   };
 };
+
+const anotherCounter = counterFactory();
+console.log("decrement: ", anotherCounter.decrement()); // -1
+
+console.log("increment: ", anotherCounter.increment()); // 0
+console.log("increment: ", anotherCounter.increment()); // 1
+console.log("increment: ", anotherCounter.increment()); // 2
+
+console.log("decrement: ", anotherCounter.decrement()); // 1
+console.log("decrement: ", anotherCounter.decrement()); // 0
+
+// ==== Challenge 4: Repeat Challenge 3 with an IIFE ====
+console.log("==== Challenge 4: Repeat Challenge 3 with an IIFE ====");
+const someCounter = (() => {
+   // Initialized counter to 20 to show a difference in the logs
+   let counter = 20;
+
+   return {
+      increment: () => ++counter,
+      decrement: () => --counter
+   };
+})();
+
+console.log("decrement: ", someCounter.decrement()); // 19
+
+console.log("increment: ", someCounter.increment()); // 20
+console.log("increment: ", someCounter.increment()); // 21
+console.log("increment: ", someCounter.increment()); // 22
+
+console.log("decrement: ", someCounter.decrement()); // 21
+console.log("decrement: ", someCounter.decrement()); // 20
+
